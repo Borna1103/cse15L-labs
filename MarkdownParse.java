5e03261f42f8867aca92898d4b44b5c7acf0c965
Borna1103 <Borna1103@msn.com>
2022-05-08 21:15:35 -0700

    .

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -9,13 +9,17 @@ public class MarkdownParse {
         // find the next [, then find the ], then find the (, then read link upto next )
         int currentIndex = 0;
         while(currentIndex < markdown.length()) {
-            if(markdown.indexOf("[", currentIndex-1) == -1){return toReturn;}
+	    if(markdown.indexOf("[", currentIndex-1) == -1){
+		    
+		    return toReturn;}
             int openBracket = markdown.indexOf("[", currentIndex);
             int closeBracket = markdown.indexOf("]", openBracket);
             int openParen = markdown.indexOf("(", closeBracket);
             int closeParen = markdown.indexOf(")", openParen);
-            toReturn.add(markdown.substring(openParen + 1, closeParen));
-            currentIndex = closeParen + 1;
+	    
+	    toReturn.add(markdown.substring(openParen + 1, closeParen));
+	         currentIndex = closeParen + 1;  
+	
         }
         return toReturn;
     }
