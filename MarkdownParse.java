808991a66dd9c517092a9e2038e800e203fd1d60
Borna1103 <Borna1103@msn.com>
2022-05-26 01:54:24 -0700

    yes

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -11,29 +11,18 @@ public class MarkdownParse {
         int currentIndex = 0;
         while(currentIndex < markdown.length()) {
 	    if(markdown.indexOf("[", currentIndex-1) == -1){
-<<<<<<< HEAD
-		    
-=======
 		    if(toReturn.size() == 0){
 			    throw new StringIndexOutOfBoundsException();
 		    }
->>>>>>> daccc1d58fe2010d38e9d395712a4a687effb0ff
 		    return toReturn;}
             int openBracket = markdown.indexOf("[", currentIndex);
             int closeBracket = markdown.indexOf("]", openBracket);
             int openParen = markdown.indexOf("(", closeBracket);
             int closeParen = markdown.indexOf(")", openParen);
-<<<<<<< HEAD
-	    
-	    toReturn.add(markdown.substring(openParen + 1, closeParen));
-	         currentIndex = closeParen + 1;  
-	
-=======
 	    if(markdown.substring(openParen + 1, closeParen).length() == 0){currentIndex = closeParen + 1;}
 	    else{toReturn.add(markdown.substring(openParen + 1, closeParen));
 	         currentIndex = closeParen + 1;  
 		}
->>>>>>> daccc1d58fe2010d38e9d395712a4a687effb0ff
         }
         return toReturn;
     }
--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,6 +1,7 @@
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
+import java.beans.Transient;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -37,5 +38,40 @@ public class MarkdownParseTest{
         try{MarkdownParse.getLinks(content);
             fail();
         }catch(StringIndexOutOfBoundsException e){} 
+    }
+
+    @Test
+    public void snip1() throws IOException{
+        Path fileName = Path.of("C:/Users/Borna Hemmaty/OneDrive/Documents/GitHub/cse15L-labs/snip1.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(content);
+        ArrayList<String> expected = new ArrayList<String>();
+        expected.add("google.com");
+
+        assertEquals(expected, links);
+    }
+
+    @Test
+    public void snip2() throws IOException{
+        Path fileName = Path.of("C:/Users/Borna Hemmaty/OneDrive/Documents/GitHub/cse15L-labs/snip2.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(content);
+        ArrayList<String> expected = new ArrayList<String>();
+        expected.add("a.com");
+        expected.add("a.com(())");
+        expected.add("example.com");
+
+        assertEquals(expected, links);
+    }
+
+    @Test
+    public void snip3() throws IOException{
+        Path fileName = Path.of("C:/Users/Borna Hemmaty/OneDrive/Documents/GitHub/cse15L-labs/snip3.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(content);
+        ArrayList<String> expected = new ArrayList<String>();
+        expected.add("https://sites.google.com/eng.ucsd.edu/cse-15l-spring-2022/schedule");
+
+        assertEquals(expected, links);
     }
 }
